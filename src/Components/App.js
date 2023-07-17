0c64897587399bf1f5863a6370c69ee94c18fae7
Gaurav Gautam <gautamgaurav945@gmail.com>
2023-07-17 18:34:39 +0530

    added some features

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,6 +10,7 @@ import Footer from "./Footer"
 
 
 
+
 function App() {
   return (
     
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React,{useRef} from 'react'
 // import image from  
+import {motion} from "framer-motion"
+
+
 
 const Header = () => {
+  const header=useRef(null);
+
+  window.onscroll=()=>{
+
+    const rect = header.current.getBoundingClientRect();
+    var num=window.scrollY;
+    var mtop=rect.top+num;
+    console.log(num,mtop);
+    if(header.current){ 
+
+      if(num>=100){
+        header.current.style.backgroundColor="#302e2e";
+      }
+else{
+  header.current.style.backgroundColor="black";
+
+}
+var val=690;
+
+
+console.log(700-num);
+    if(num>=650){
+      header.current.style.position="absolute";
+  header.current.style.marginTop=`${val-num}rem`
+    }else {
+      header.current.style.position="fixed";
+      header.current.style.marginTop="0px"
+    }
+   }
+                  
+  
+  }
+
   return (
     <>
       
-<div id="container">
+<div id="container"  ref={header}>
 
 <div ><img id="logo" src="../Images/logo.png" /></div>
 <div id="navbar">
@@ -14,7 +50,11 @@ const Header = () => {
 <div className="font-larger" id="Our Value">Our Value</div>
 <div className="font-larger" id="Contact Us">Contact Us</div>
 <div className="font-larger" id="Get Started">Get Started</div>
-<button id="contact">Contact</button>
+<motion.button
+
+whileHover={{scale:1.1}}
+transition={{duration:"0.5"}}
+id="contact">Contact</motion.button>
 
 </div>
 
