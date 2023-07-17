29d911c7fcea502bd28239c2e5863021211d70e0
Gaurav Gautam <gautamgaurav945@gmail.com>
2023-07-17 15:37:44 +0530

    added ref hook

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,57 +1,118 @@
-import React from 'react'
-import json from "../slider.json"
-import Part from "./part.jsx"
+// import React from 'react'
+// import json from "../slider.json"
+// import Part from "./part.jsx"
+// const Slider = () => {
+
+// var rcontainer=document.getElementById("rscrollcontainer");
+
+
+
+// const right=()=>{
+// rcontainer.scrollBy({
+//     left: -290,
+//     right: 0,
+//     // behavior: "smooth"
+
+// })
+// }
+
+// const left=()=>{
+//     rcontainer.scrollBy({
+//         left: 290,
+//         right: 0,
+//         // behavior: "smooth"   
+//     })
+// }
+
+//   return (
+//     <>
+
+// <div id="rright" onClick={right}><span id="sliderleft" className="material-symbols-outlined">
+// chevron_left
+// </span></div>
+// <div id="rleft" onClick={left}><span  id="sliderright" className="material-symbols-outlined">
+// chevron_right
+// </span></div>
+
+//      <div id="rscrollcontainer">
+// {json.map((ele,index)=>{
+// return(
+
+//     <Part
+//     key={index}
+//     name={ele.name}
+//     price={ele.price}
+//     detail={ele.detail}
+//     image={ele.image}
+    
+//     />
+// )
+// })}
+
+// </div>
+//    </>
+//   )
+// }
+
+// export default Slider
+
+//
+//
+
+
+import React, { useRef } from 'react';
+import json from '../slider.json';
+import Part from './part.jsx';
+
 const Slider = () => {
+  const rcontainerRef = useRef(null);
 
-var rcontainer=document.getElementById("rscrollcontainer");
+  const right = () => {
+    if (rcontainerRef.current) {
+      rcontainerRef.current.scrollBy({
+        left: -290,
+        behavior: 'smooth'
+      });
+    }
+  };
 
-
-console.log(rcontainer);
-const right=()=>{
-rcontainer.scrollBy({
-    left: -290,
-    right: 0,
-    // behavior: "smooth"
-
-})
-}
-
-const left=()=>{
-    rcontainer.scrollBy({
+  const left = () => {
+    if (rcontainerRef.current) {
+      rcontainerRef.current.scrollBy({
         left: 290,
-        right: 0,
-        // behavior: "smooth"   
-    })
-}
+        behavior: 'smooth'
+      });
+    }
+  };
 
   return (
     <>
+      <div id="rright" onClick={right}>
+        <span id="sliderleft" className="material-symbols-outlined">
+          chevron_left
+        </span>
+      </div>
+      <div id="rleft" onClick={left}>
+        <span id="sliderright" className="material-symbols-outlined">
+          chevron_right
+        </span>
+      </div>
 
-<div id="rright" onClick={right}><span id="sliderleft" class="material-symbols-outlined">
-chevron_left
-</span></div>
-<div id="rleft" onClick={left}><span  id="sliderright" class="material-symbols-outlined">
-chevron_right
-</span></div>
+      <div id="rscrollcontainer" ref={rcontainerRef}>
+        {json.map((ele, index) => {
+          return (
+            <Part
+              key={index}
+              name={ele.name}
+              price={ele.price}
+              detail={ele.detail}
+              image={ele.image}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
-     <div id="rscrollcontainer">
-{json.map((ele,index)=>{
-return(
-
-    <Part
-    key={index}
-    name={ele.name}
-    price={ele.price}
-    detail={ele.detail}
-    image={ele.image}
-    
-    />
-)
-})}
-
-</div>
-   </>
-  )
-}
-
-export default Slider
+export default Slider;
