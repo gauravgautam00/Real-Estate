aad554642c3514d34cde9c36833bcfad5f37d696
Gaurav Gautam <gautamgaurav945@gmail.com>
2023-07-20 01:29:19 +0530

    adding responsive code

--- src/Components/Front_Page.jsx
+++ src/Components/Front_Page.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , {useRef,useEffect} from 'react'
 import Header from './Header'
 import LeftPart from "./Leftpart"
 import RightPart from "./Rightpart"
@@ -12,11 +12,17 @@ backgroundColor:"black",
 paddingTop:"0.1px",
 }
 const Front_Page = () => {
+
+
+
+
+
   return (
     <>
   
-    <div id="frontpagee"style={style}>
+    <div id="frontpagee" style={style}  >
       <div>
+      
       <Header
 name="Restaurant"
 path="rest"
